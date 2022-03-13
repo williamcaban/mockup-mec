b4787083f1b1f48184d220c5404ac0d271f180d1
William Caban <william.caban@gmail.com>
2022-03-13 18:27:07 -0400

    Updates optimizing container build and additional pip3 modules

--- build-container.sh
+++ build-container.sh
@@ -2,7 +2,7 @@
 export BASEREG=quay.io/wcaban
 export DTAG=$(date +%y%m%d)
 
-podman build -t ${BASEREG}/mockup-mec -f containers/Containerfile
+podman build -t ${BASEREG}/mockup-mec -f containers/Containerfile .
 # podman tag      ${BASEREG}/mockup-mec:latest ${BASEREG}/mockup-mec:${DTAG}
 # podman push     ${BASEREG}/mockup-mec:latest
 # podman push     ${BASEREG}/mockup-mec:${DTAG}
