228a57fb60d1c3f87edb97e0f4186c77049778da
William Caban <william.caban@gmail.com>
2022-03-13 17:29:53 -0400

    Fixing ordering

--- build-container.sh
+++ build-container.sh
@@ -3,6 +3,6 @@ export BASEREG=quay.io/wcaban
 export DTAG=$(date +%y%m%d)
 
 podman build -t ${BASEREG}/mockup-mec -f containers/Containerfile
-podman tag      ${BASEREG}/mockup-mec:latest ${BASEREG}/mockup-mec:${DTAG}
-podman push     ${BASEREG}/mockup-mec:latest
-podman push     ${BASEREG}/mockup-mec:${DTAG}
+# podman tag      ${BASEREG}/mockup-mec:latest ${BASEREG}/mockup-mec:${DTAG}
+# podman push     ${BASEREG}/mockup-mec:latest
+# podman push     ${BASEREG}/mockup-mec:${DTAG}
