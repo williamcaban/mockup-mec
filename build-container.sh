30382c84520a0556584091e8afbc82e68533488f
William Caban <william.caban@gmail.com>
2022-03-13 19:22:34 -0400

    Optimizations for container build and scripts

--- build-container.sh
+++ build-container.sh
@@ -1,8 +1,13 @@
 # Script to build container
+
 export BASEREG=quay.io/wcaban
 export DTAG=$(date +%y%m%d)
+#export BUILD_AND_UPLOAD=True
 
 podman build -t ${BASEREG}/mockup-mec -f containers/Containerfile .
-# podman tag      ${BASEREG}/mockup-mec:latest ${BASEREG}/mockup-mec:${DTAG}
-# podman push     ${BASEREG}/mockup-mec:latest
-# podman push     ${BASEREG}/mockup-mec:${DTAG}
+
+if [ "$BUILD_AND_UPLOAD" == "True" ]; then
+    podman tag      ${BASEREG}/mockup-mec:latest ${BASEREG}/mockup-mec:${DTAG}
+    podman push     ${BASEREG}/mockup-mec:latest
+    podman push     ${BASEREG}/mockup-mec:${DTAG}
+fi
